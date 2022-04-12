1643e6a2ef209e6fb9c50688bfbc620b5aab2363
Krishna-500apps <krishnaiah.malli@mantragroup.com>
2022-04-12 18:03:50 +0530

    practice on router

---
+++ proj1/src/components/test.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <h1>router</h1>
+      <button @click='f1()'>click</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'ROut1',
+    data(){
+        return{
+            a:[3,4,6,7,8,9]
+
+        }
+
+    }, 
+    methods:{
+        f1(){
+            for(var i=0;i<this.a.length;i++){
+                if(this.a[3]==9){
+                this.a[3]=9
+                    document.write(this.a)
+                }
+            }
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
