8a1ab83d66c57eeda746ef64c784b97513194a87
Krishna-500apps <krishnaiah.malli@mantragroup.com>
2022-04-13 18:51:22 +0530

    components

---
+++ proj1/src/components/13/04/2022/emitchild.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <button @click="sample()">sample click</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"EmitEvent",
+    components:{} ,
+    data(){
+        return{
+            fields:['id','name','dept'],
+            items:[{id:'1175',name:'sai',dept:'developer'},
+                    {id:'1176',name:'venkat',dept:'developer'},
+                    {id:'1177',name:'mahesh',dept:'developer'},
+                    {id:'1178',name:'krishna',dept:'developer'},
+                    ],
+            
+        }
+    },
+     methods:{
+                   sample(){
+                       this.$emit("check")
+                       console.log("child")
+                   }
+
+               }
+
+}
+</script>
+
---
+++ proj1/src/components/13/04/2022/emitparent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+      <b-table :items="items" :fields="fields"/>
+      <emitchild @check="f1()"></emitchild>
+
+  </div>
+</template>
+
+<script>
+import emitchild  from'../components/emitchild.vue'
+export default {
+    name:"EmiT",
+    components:{ 
+        emitchild,
+    },
+    data(){
+        return{
+            fields:['id','name','dept'],
+            items:[
+                {id:'1175',name:'sai',dept:'developer'},
+                    {id:'1176',name:'venkat',dept:'developer'},
+                    {id:'1177',name:'mahesh',dept:'developer'},
+                    {id:'1178',name:'krishna',dept:'developer'},
+                    ],
+
+            }
+        },
+        methods:
+            {
+                f1(){
+                    console.log("sample check emit");
+                }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
