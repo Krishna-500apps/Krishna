f0adedd78e1f91f9c28601dcfe6ab9259463b9c2
Krishna-500apps <krishnaiah.malli@mantragroup.com>
2022-04-19 19:15:43 +0530

    practice on 19

---
+++ proj1/src/components/19/04/callback.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <h3>callback</h3>
+      <button @click="sum()"> result </button>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name:"CAllback",
+    data(){
+        return{
+            result:this.mycalculator(4,5,this.mydisplayer),
+    
+        };
+    },
+    methods:{
+        mydisplayer:(something)=>{document.write(something);
+        },
+        mycalculator:(arg1,arg2,arg3)=>{
+            let sum=arg1+arg2;
+            arg3(sum);
+        }
+   
+    }   
+}
+
+
+    
+</script>
+
+<style>
+
+</style>
---
+++ proj1/src/components/axios.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+      <h3> axios</h3>
+      <button @click="fun1()">click</button>
+  </div>
+</template>
+
+<script>
+const axios=require('axios').default;
+export default {
+
+    name:"AxioS",
+    data(){
+        return{
+            posts:"",
+           // fields:["name","country","web-pages"]
+        }
+    },
+    methods:{
+        async fun1(){
+            let response=await axios.get("http://universities.hipolabs.com/search?country=")
+            //this.posts=await response.json()
+            console.log(response)
+        },
+    },
+     created(){
+        console.log(this.posts);
+
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
