715040e2212a4d6776adc316e8875fc45a671048
Krishna-500apps <krishnaiah.malli@mantragroup.com>
2022-04-20 19:11:46 +0530

    20-04-2022

---
+++ proj1/src/components/20/20/04 Assgn/Que1.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+      <input name="myform" method="post" onsubmit="fun1()">
+      <inpt type="text" name="words" placeholder="Entertext">
+          <button @click="fun1()">serch</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+           // post:"",
+           words:"gi",
+            array:["agile","crm","mantra","technologies","500apps","infinity"]
+
+
+           
+        }
+    },
+    methods:{
+        fun1(){
+            
+            
+
+            
+        }
+
+    }
+    
+    
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ proj1/src/components/20/20/04 Assgn/Que4.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <h3>fetch api</h3>
+      <button @click="getData()">click</button>
+       <b-form-input v-model="value" placeholder=" Enter your company name"></b-form-input>
+      <b-table striped hover :items="posts" :fields="fields"></b-table>
+  </div>
+</template>
+
+<script> 
+export default {
+name:"QUe4",
+data(){
+    return{
+    posts:" ",
+        fields:["name","email"]
+    }
+},
+methods:{
+async getData(){
+
+  let response=await fetch("http://www.json-generator.com/api/json/get/bSldINskRK=" +this.value)
+this.posts=await response.json()
+}
+},
+created(){
+    this.getData();
+}
+}
+</script>
