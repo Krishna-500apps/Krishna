60b2a1e9265787af8c019274df6d21f99483970e
Krishna-500apps <krishnaiah.malli@mantragroup.com>
2022-04-25 19:02:03 +0530

    assgn25-04

---
+++ proj1/src/components/Assgn4/Qn8.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <h4>filter the data</h4>
+      <button @click="fun1()">filter</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"QN8",
+    data(){
+        return{
+             officers:[
+                          { id: 20, name: 'Captain Piett' },
+                          { id: 24, name: 'General Veers' },
+                          { id: 56,name: 'Admiral Ozzel' },
+                          { id: 88, name: 'Commander Jerjerrod'},
+                ],
+        }
+    },
+    methods:{
+        fun1(){
+            var officers=this.officers.filter((officers)=>{
+                return officers.name==("General Veers")
+            });
+            console.log(officers)
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
