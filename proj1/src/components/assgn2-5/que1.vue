a559fb76dcab7371b13dd78ec36e70a477ca6de6
Krishna-500apps <krishnaiah.malli@mantragroup.com>
2022-05-02 18:31:06 +0530

    assgn2-05

---
+++ proj1/src/components/assgn2-5/que1.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <h1>output of the function</h1>
+      <p>{{perimeter()}}</p>
+      <p>{{diameter()}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"QuE1",
+    data(){
+        return{
+            radius:20,
+            PI:3.14,
+            perimeter: () => 2* Math.PI * this.radius,
+            diameter:()=>this.radius*2
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ proj1/src/components/assgn2-5/que8.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+      <h5>sequence order</h5>
+      <button @click="fun8()">order</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"QuE8",
+    data(){
+        return{
+             color : ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "]
+        }
+    },
+    methods:{
+       fun8(){
+          function Ordinal(n)
+           {
+             var o = ["th","st","nd","rd"],
+              x = n%100;
+              return x+(o[(x-20)%10]||o[x]||o[0]);
+            }
+             for(this.n = 0; this.n < this.color.length; this.n++){
+             var ordinal = this.n + 1;
+            var output = (Ordinal(ordinal) + " choice is " + this.color[this.n] + ".");
+               console.log(output);
+            }
+        }
+
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
