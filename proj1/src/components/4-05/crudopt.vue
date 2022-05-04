4f51cfc0d81cbc26e2679ee000ab57d51dd75645
Krishna-500apps <krishnaiah.malli@mantragroup.com>
2022-05-04 18:57:29 +0530

    04-05

---
+++ proj1/src/components/4-05/crudopt.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <b-form id="form">
+      Enter FirstName:<b-form-input v-model="value" placeholder="enter First name" ></b-form-input>
+      Enter LastName:<b-form-input v-model="value1" placeholder="enter Last name" ></b-form-input>
+      <b-button @click="Add()" variant="success">Add</b-button>
+      <b-button @click="Delete()" variant="danger">Delete</b-button>
+      <b-button @click="Update()" variant="primary">Update</b-button>
+    </b-form>
+    <b-table striped hover :items="items" :fields="fields"></b-table>
+  </div>
+</template>
+<script>
+export default {
+  name: "QuestoN3",
+
+      data() {
+       return {
+      items: [{ first_name: "krishna", last_name: "malli" }],
+      fields: ["first_name", "last_name"],
+      value: "",
+      value1: "",
+    };
+  },
+  methods: {
+    Add() {
+      this.items.push({
+        first_name: this.value,
+
+        last_name: this.value1,
+      });
+
+      (this.value = ""), (this.value1 = "");
+    },
+
+    Delete() {
+      this.items.pop();
+    },
+
+    Update() {
+      this.items.put({
+        first_name: this.value,
+
+        last_name: this.value1,
+      });
+
+      (this.value = ""), (this.value1 = "");
+    },
+  },
+};
+</script>
+
