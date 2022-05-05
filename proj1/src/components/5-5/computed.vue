461828e34e3a588a44af86fb3933374037a3c9d9
Krishna-500apps <krishnaiah.malli@mantragroup.com>
2022-05-05 19:52:42 +0530

    05-05

---
+++ proj1/src/components/5-5/computed.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+      <h5>computed</h5>
+    
+    <button v-on:click="a++">Add to A</button>
+      <button v-on:click="b--">Add to B</button>
+      <p>A-{{a}}</p>
+      <p>B-{{b}}</p>
+      <p>Age + A={{addToA}}</p>
+      <p>Age + B={{addToB}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"ComputeD",
+    data(){
+        
+        return{
+             age:20,
+             a:0,
+             b:0,
+        }
+    },
+methods:{
+        //   addToA:function(){
+        //       console.log('addToA')
+        //     return this.a + this.age;
+        
+        //    },
+        //     addToB:function(){
+        //       console.log('addToB')
+        //     return this.b + this.age
+        //  }
+        },
+        computed:{
+            addToA:function(){
+              console.log("hi")
+            return this.a + this.age;
+        
+           },
+            addToB:function(){
+              console.log("a")
+            return this.b + this.age
+         }
+        }
+      }  
+    
+
+
+</script>
+
+<style>
+
+</style>
