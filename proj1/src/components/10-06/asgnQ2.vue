804e526513684eb3a378cebac902e042766b87b8
Krishna-500apps <krishnaiah.malli@mantragroup.com>
2022-05-10 20:07:17 +0530

    assgn10-05

---
+++ proj1/src/components/10-06/asgnQ2.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      string1:<input type="text" v-model="String1"><br/>
+      string2:<input type="text" v-model="String2">
+  </div>
+</template>
+
+<script>
+export default {
+    name:"AsgQ",
+    data(){
+        return{
+            String1:"",
+            String2:""
+        }
+    },
+    watch:{
+        String1:function(row){
+            this.String1=row;
+            this.String2=this.String1.toUpperCase();
+        }
+    }
+
+    
+
+}
+</script>
+
+<style>
+
+</style>
