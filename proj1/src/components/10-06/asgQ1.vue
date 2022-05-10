e5a2b40fe5e321dec6c74e79208274934910a684
Krishna-500apps <krishnaiah.malli@mantragroup.com>
2022-05-10 17:37:38 +0530

    asgn 10-05

---
+++ proj1/src/components/10-06/asgQ1.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <b-form id="form">
+        Enter product name:<b-form-input id="name" class="name" placeholder="Enter product name:" ></b-form-input>
+      Enter product price:<b-form-input id="price" class="prices" placeholder="Enter product price:"></b-form-input>
+     Enter product category:<b-form-input id="category" class="categories" placeholder="Enter product category"></b-form-input>
+ <b-button @click="Add()">Add</b-button><br /><br />
+    </b-form>
+<b-table striped hover :items="items" :fields="fields"></b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AsgQ1",
+
+  data() {
+    return {
+      items: [
+        {
+          productname: "shirt",
+          productprice: "100",
+          productcategory: "cloths",
+        },
+      ],
+
+      fields: [],
+    };
+  },
+
+  methods: {
+    Add() {
+      this.items.push({
+        productname: document.getElementById("name").value,
+        productprice: document.getElementById("price").value,
+        productcategory: document.getElementById("category").value,
+      });
+    }
+  }
+}
+</script>
+
+
